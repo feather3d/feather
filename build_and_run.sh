160a8aac153520b3ddedb1e8037a7eae84a1f816
mottosso <konstruktion@gmail.com>
2015-12-02 18:20:38 +0000

    Tabs to spaces

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,11 +4,11 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/feather
 CWD=$CD  # Remember
 
 cd build && \
-cmake .. && \  				# Append new files
+cmake .. && \               # Append new files
 make && \
 sudo make install && \
 cd /usr/local/feather && \  # Feather is installed here
-./feather || \				# Run, or indicate failure
+./feather || \              # Run, or indicate failure
 echo Failed
 
 cd $CWD  # Restore
