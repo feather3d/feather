cd62ee059f9948c5a6c30d2a19d0142009d61c20
mottosso <konstruktion@gmail.com>
2015-12-02 20:57:03 +0000

    Removing logic; didn't work too great.

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,12 +3,11 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/feather
 
 CWD=$CD  # Remember
 
-cd build && \
-cmake .. && \               # Append new files
-make && \
-sudo make install && \
-cd /usr/local/feather && \  # Feather is installed here
-./feather || \              # Run, or indicate failure
-echo Failed
+cd build
+cmake ..               # Append new files
+make
+sudo make install
+cd /usr/local/feather  # Feather is installed here
+./feather              # Run, or indicate failure
 
 cd $CWD  # Restore
